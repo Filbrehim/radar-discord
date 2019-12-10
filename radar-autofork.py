8ac077030b762d379aa01ddd82feca9adfe74582
Filbrehim <malignialourdsabot@gmail.com>
2019-12-10 11:27:39 +0100

    réponse détaillée dans un message privé

--- radar-autofork.py
+++ radar-autofork.py
@@ -37,16 +37,17 @@ host=os.uname().nodename
 jeu = discord.Game("{0} sur {1}".format(moi,host))
 client = discord.Client()
 
-async def afficher(res,demande,message) :
+async def afficher(res,demande,message,channel) :
    if res['score'] :
       print (" ** {s}: {texte}".format(s=res['score'],texte=res['type']),file=flog)
       await message.channel.send("__{0}__ a un score de **{1}**".format(demande,res['score']))
+      await channel.send("__{0}__ a un score de **{1}**".format(demande,res['score']))
       for champ in ['type','compact','vendeur'] :
           if champ in res.keys() :
-              await message.channel.send("{0} : **{1}**".format(champ,res[champ])) 
+              await channel.send("{0} : **{1}**".format(champ,res[champ])) 
       if 'detail' in res.keys() :
           for d in res['detail'] :
-              await message.channel.send("{0} : **{1}**".format(d,res['detail'][d]))
+              await channel.send("{0} : **{1}**".format(d,res['detail'][d]))
 
 async def webhook(res,demande,message) :
    if res['score'] == 0 : return 
@@ -57,10 +58,10 @@ async def webhook(res,demande,message) :
       if os.path.exists(channel_dir+"/webhook.txt") :
          a=1
       else :
-         await afficher(res,demande,message)
+         await afficher(res,demande,message,message.channel)
    else :
       os.mkdir(channel_dir)
-      await afficher(res,demande,message)
+      await afficher(res,demande,message,message.channel)
 
 @client.event
 async def on_ready():
@@ -87,11 +88,15 @@ async def on_message(message):
 
     if message.content.lower().startswith(moi.lower()+' '):
         public=True
+        channel_answer = message.channel
         if type(message.channel) == discord.channel.DMChannel :
             print('un message pour moi [{0.content}] de {0.author.name}, en privé'.format(message),file=flog)
             public = False
         else :
             print('un message pour moi [{0.content}] de {0.author.name} sur {0.channel.name}'.format(message),file=flog)
+            channel_answer = message.author.dm_channel 
+            if channel_answer == None :
+                 channel_answer = await message.author.create_dm()
         content = message.content[len(moi)+1:]
         await message.channel.send('{1.author.name} me demande **{0}**'.format(content,message))
         async with message.channel.typing() :
@@ -99,9 +104,9 @@ async def on_message(message):
              if len(demande) < 7 : 
                 continue
              for res in [ macaddr.chercher(demande), sanwwid.chercher(demande), timestamp.chercher(demande) ] :
-                 if public and os.path.isdir(moi+".dir") :
+                 if False and public and os.path.isdir(moi+".dir") :
                     await webhook(res,demande,message)
                  else : 
-                    await afficher(res,demande,message) 
+                    await afficher(res,demande,message,channel_answer) 
 
 client.run(get_discord_token()) 
